cb6796e82bebeffe088ac357a7c9784fff92bcf5
Zhiqi Huang <Charles.hzq@gmail.com>
2018-12-09 19:47:17 -0500

    olken exp

--- sample_join.py
+++ sample_join.py
@@ -1,7 +1,15 @@
 import pandas as pd
-import random
+import numpy as np
 import os
 
+"""
+Random State 
+"""
+rd = np.random.RandomState(42)
+
+"""
+Load Table
+"""
 cur_dir = os.path.dirname(os.path.realpath(__file__))
 cust_table = pd.read_table(os.path.join(cur_dir, "data", "0.1x", "customer.tbl"), delimiter='|', usecols=[0], names=["CUSTKEY"])
 order_table = pd.read_table(os.path.join(cur_dir, "data", "0.1x", "orders.tbl"), delimiter='|', usecols=[0, 1], names=["ORDERKEY", "CUSTKEY"])
@@ -14,28 +22,44 @@ lineitem_table = pd.read_table(os.path.join(cur_dir, "data", "0.1x", "lineitem.t
 def getMaxFreq(table, col):
     return list(table[col].value_counts().to_dict().values())[0]
 
-def olken(lst):
-    W = 0.0
-    Wp = 0.0
-    rej_prob = 0.0
-    sample_prob = 0.0
-    table1 = lst[0]
-    table2 = lst[1]
-    table3 = lst[2]
-    size1 = table1.shape[0]
-    max2 = getMaxFreq(table2, "CUSTKEY")
-    max3 = getMaxFreq(table3, "ORDERKEY")
-    W = size1 * max2 * max3
-    Wp = W
-    rej_prob = 1 - W/Wp
-    if (random.uniform(0, 1) > rej_prob):
-        # sample
+def getFreq(table, col, value):
+    return (table.iloc[:,col] == value).sum()
 
-    else:
-        pass
-    
-    print(W)
+def olken(lst, frame):
+    """
+    Init
+    """
+    output = []
+    p = 1.0
+    level = 1
+
+    """
+    First table
+    """
+    idx = rd.randint(0, lst[0].shape[0])
+    value = lst[0].iloc[idx,frame[0][0]]
+    output.append(value) #i.e Q3, this is c.custkey
+
+    while level < len(lst):
+
+        degree = getFreq(lst[level],frame[level][0],value)
+        idx_offset = rd.randint(0, degree)
+        matched = lst[level][lst[level].iloc[:,frame[level][0]] == value].iloc[idx_offset]
+        
+        """
+        Update value
+        """
+
+        value = matched[frame[level][1]]
+
+        output.append(value)
+
+        level += 1
+
+        p *= degree
+
+    print(output, p)
 
 
-
-olken([cust_table, order_table, lineitem_table])
+olken([cust_table,order_table,lineitem_table],[(0,0),(1,0),(0,1)])
+print(getMaxFreq(lineitem_table,'ORDERKEY'))
