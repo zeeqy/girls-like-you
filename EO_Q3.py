ee60dc8dd4fc77fb888de38e754ad7566ab422ae
Puxuan Yu <pxyuwhu@gmail.com>
2018-12-16 14:34:36 -0500

    Update EO_Q3.py

--- EO_Q3.py
+++ EO_Q3.py
@@ -8,14 +8,16 @@ import sys
 def getMaxFreq(table, col):
     return list(table.iloc[:,col].value_counts().to_dict().values())[0]
 
+def freq_table(table, col):
+    return table.iloc[:,col].value_counts().to_dict()
+
 def getFreq(table, col, value):
     return (table.iloc[:,col] == value).sum()
 
-def olken(lst, frame, max_p):
+def olken(lst, lst2, frame, max_p):
     """
     Init
     """
-    output = []
     p = max_p = 1.0
     level = 1
 
@@ -24,25 +26,28 @@ def olken(lst, frame, max_p):
     """
     idx = np.random.randint(0, lst[0].shape[0])
     value = lst[0].iloc[idx,frame[0][0]]
-    output.append(value) #i.e Q3, this is c.custkey
 
     while level < len(lst):
+        # table dataframe
+        df = lst[level]
+        # frequency dataframe
+        df2 = lst2[level-1]
+        # frame in current iteration
+        cur_frame = frame[level]
 
-        degree = getFreq(lst[level],frame[level][0],value)
-        
-        # zero matching
-        if not degree:
-
+        if value not in df2:
+            # zero matching
             return False
+        else:
+            degree = df2[value]
 
         idx_offset = np.random.randint(0, degree)
-        matched = lst[level][lst[level].iloc[:,frame[level][0]] == value].iloc[idx_offset]
         
         """
         Update value
         """
-        value = matched[frame[level][1]]
-        output.append(value)
+        attr = list(df.columns.values)[cur_frame[0]]
+        value = df[df[attr]==value].iloc[idx_offset,cur_frame[1]]
         level += 1
         p *= degree
 
@@ -52,13 +57,20 @@ def main():
     """
     Parsing Arguments
     """
-    parser = argparse.ArgumentParser(description="take params including scale factors")
+    parser = argparse.ArgumentParser(description="take params including scale factors and sample size")
     parser.add_argument('--sf', nargs=1)
+    parser.add_argument('--ss', nargs=1)
     if (parser.parse_args().sf == None):
         print("Please specify scale factor after --sf\ne.g. \"python EO_Q3.py --sf 0.1\"")
         exit(0)
     else:
         sf = parser.parse_args().sf[0]
+
+    if (parser.parse_args().ss == None):
+        print("Please specify sample size after --ss\ne.g. \"python EO_Q3.py --ss 10000\"")
+        exit(0)
+    else:
+        tot_size = int(parser.parse_args().ss[0])
     
     """
     Random State 
@@ -77,34 +89,31 @@ def main():
     order_table = pd.read_table(os.path.join(cur_dir, "data", sf + "x", "orders.tbl"), delimiter='|', usecols=[0, 1], names=["ORDERKEY", "CUSTKEY"])
     lineitem_table = pd.read_table(os.path.join(cur_dir, "data", sf + "x", "lineitem.tbl"), delimiter='|', usecols=[0, 3], names=["ORDERKEY", "LINENUMBER"])
 
+    order_cust_freq = freq_table(order_table, 1)
+    line_order_freq = freq_table(lineitem_table, 0)
+    
     """
     Prepare to sample
     """
-    tot_size = 1000
     sample_size = 0
     max_p = 1.0
     frame = [(0,0),(1,0),(0,1)]
-    lst = [cust_table,order_table,lineitem_table]
+    lst_table = [cust_table,order_table,lineitem_table]
+    lst_freq = [order_cust_freq, line_order_freq]
 
     for tbl in range(1,len(frame)):
-        max_p *= getMaxFreq(lst[tbl],frame[tbl][0])
+        max_p *= getMaxFreq(lst_table[tbl],frame[tbl][0])
 
     """
     Begin sampling
     """
     start_time = time.time()
     
-    while sample_size <= tot_size:
-        if olken(lst,frame,max_p):
+    while sample_size < tot_size:
+        if olken(lst_table,lst_freq,frame,max_p):
             sample_size += 1
 
     print("--- %s seconds ---" % (time.time() - start_time))
 
-
-
 if __name__ == '__main__':
     main()
-
-
-
-
